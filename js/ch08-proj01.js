63a0c4ff3f8525711d6e7df48af70b6805e06020
lilvuzy <112009814+lilvuzy@users.noreply.github.com>
2022-10-11 18:47:50 -0400

    Add files via upload

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -1,6 +1,26 @@
-
-const tax_rate = prompt('Enter tax rate (0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (1000)');
+const tax_rate = prompt("Enter tax rate (0.10)");
+const shipping_threshold = prompt("Enter shipping threshold (1000)");
+let subTotal = 0;
+let tax = 0;
+let grandTotal = 0;
+let shipping = 0;
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+cart.forEach((cart) => {
+  subTotal = subTotal + calculateTotal(cart.quantity, cart.product.price);
+  outputCartRow(cart, calculateTotal(cart.quantity, cart.product.price));
+});
+
+if (subTotal > shipping_threshold) {
+  let shipping = 0;
+} else {
+  let shipping = 40;
+}
+
+tax = calculateTax();
+shipping = calculateShipping();
+grandTotal = subTotal + tax;
+
+outputCosts();
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,48 @@
 /* define your functions here */
+function calculateTotal(quantity, price) {
+  return quantity * price;
+}
 
+function outputCartRow(item, total) {
+  document.write("<tr>");
+  document.write('<td><img src="images/' + item.product.filename + '"></td>');
+  document.write("<td>" + item.product.title + "</td>");
+  document.write("<td>" + item.quantity + "</td>");
+  document.write("<td>$" + item.product.price.toFixed(2) + "</td>");
+  document.write("<td>$" + total.toFixed(2) + "</td>");
+  document.write("</tr>");
+}
 
+function calculateTax() {
+  return subTotal * tax_rate;
+}
 
+function calculateShipping() {
+  if (subTotal > shipping_threshold) {
+    return 0;
+  } else {
+    return 40;
+  }
+}
 
+function outputCosts() {
+  document.write('<tr class="totals">');
+  document.write('<td colspan="4">Subtotal</td>');
+  document.write("<td>$" + subTotal.toFixed(2) + "</td>");
+  document.write("</tr>");
 
+  document.write('<tr class="totals">');
+  document.write('<td colspan="4">Tax</td>');
+  document.write("<td>$" + tax.toFixed(2) + "</td>");
+  document.write("</tr>");
 
+  document.write('<tr class="totals">');
+  document.write('<td colspan="4">Shipping</td>');
+  document.write("<td>$" + shipping.toFixed(2) + "</td>");
+  document.write("</tr>");
 
-
-        
+  document.write('<tr class="totals">');
+  document.write('<td colspan="4" class = "focus">Grand Total</td>');
+  document.write('<td class="focus">$' + grandTotal.toFixed(2) + "</td>");
+  document.write("</tr>");
+}
